93f6be95c6e2e693327aa049d6a8e10ffed49962
Charlie Sharpsteen <source@sharpsteen.net>
2017-04-29 23:25:24 -0700

    Update documentation in RFacter::Util::DSL
    This patch updates the documentation for the RFacter::Util::DSL namespace
    to indicate which methods from the Facter 3.0 Ruby API still need to be
    implemented.

--- lib/rfacter/util/dsl.rb
+++ lib/rfacter/util/dsl.rb
@@ -5,17 +5,16 @@ require_relative '../config'
 
 # Facter compatibility layer
 #
-# This module exists to provide compatibility shims for facter DSL methods.
-# Any fact source code that is executed within the {RFacter::Util} namespace
-# via `instance_eval` should pick up on these shims. The methods in this module
-# should never be called directly, and many will raise errors.
+# This module exists to provide compatibility shims for Facter DSL methods as
+# exposed by the Facter 3.0 Ruby API. Any fact source code that is executed
+# within the {RFacter::Util} namespace via `instance_eval` should pick up on
+# these shims. The methods in this module should never be called directly.
 #
 # However, lexical scope is a tricky thing, so "should" is the operative word
 # here.
 #
 # @see https://github.com/puppetlabs/facter/blob/master/Extensibility.md
 #
-# @private
 # @since 0.1.0
 module RFacter::Util::DSL
   COLLECTION = Concurrent::ThreadLocalVar.new do
@@ -40,7 +39,25 @@ directly instead of via an instance of RFacter::Util::Collection.
 EOS
   end
 
+  # Shims for top-level Facter methods
+  #
+  # @todo Implement `[]`
+  # @todo Implement `clear`
+  # @todo Implement `define_fact`
+  # @todo Implement `each`
+  # @todo Implement `fact`
+  # @todo Implement `flush`
+  # @todo Implement `list`
+  # @todo Implement `loadfacts`
+  # @todo Implement `reset`
+  # @todo Implement `search`
+  # @todo Implement `search_path`
+  # @todo Implement `to_hash`
+  # @todo Implement `value`
+  # @todo Implement `version`
   module Facter
+    # TODO: Implement []
+    #
     # Shim for Facter.add(...)
     def self.add(name, options = {}, &block)
       COLLECTION.value.add(name, options, &block)
@@ -66,12 +83,16 @@ EOS
       ::RFacter::Config.config.logger.warnonce(msg)
     end
 
+    # Facter::Core DSL methods
+    #
+    # @todo Implement Facter::Core::Aggregate
     module Core
+      # Shims for Facter::Core::Exection methods
+      #
+      # @todo Implement which
+      # @todo Impletement ExecutionFailure (just a subclass of StandardError)
+      # @todo Implement execution options
       module Execution
-        # TODO: Implement which
-        # TODO: Impletement ExecutionFailure (just a subclass of StandardError)
-        # TODO: Implement execution options
-
         def self.exec(command)
           execute(command, :on_fail => nil)
         end
@@ -80,6 +101,13 @@ EOS
           NODE.value.execute(command).stdout.chomp
         end
       end
+    end
+
+    # Facter::Util DSL methods
+    #
+    # @todo Implement Facter::Util::Resolution
+    # @todo Implement Facter::Util::Fact
+    module Util
     end
   end
 end
