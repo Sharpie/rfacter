a06021ee3c54991e752d5d644973052f6272a47c
Charlie Sharpsteen <source@sharpsteen.net>
2017-05-09 00:09:40 -0700

    Implement remaining Facter DSL methods
    This patch implements the following methods for RFacter::DSL::Facter:
    
      - clear
      - define_fact
      - each
      - flush
      - list
      - loadfacts
      - search_path
      - to_hash
      - version
    
    Docstrings are also updated.

--- lib/rfacter/dsl.rb
+++ lib/rfacter/dsl.rb
@@ -32,19 +32,10 @@ node to execute on being set. This usually happens if the DSL method is called
 directly instead of via an instance of RFacter::Util::Collection.
 EOS
 
-  # Shims for top-level Facter methods
+  # DSL for top-level Facter methods
   #
-  # @todo Implement `clear`
-  # @todo Implement `define_fact`
-  # @todo Implement `each`
-  # @todo Implement `flush`
-  # @todo Implement `list`
-  # @todo Implement `loadfacts`
   # @todo Implement `reset`
   # @todo Implement `search`
-  # @todo Implement `search_path`
-  # @todo Implement `to_hash`
-  # @todo Implement `version`
   module Facter
     # Returns a fact object by name.
     #
@@ -59,16 +50,75 @@ EOS
       COLLECTION.value.fact(name)
     end
 
+    # Adds a {RFacter::Util::Resolution resolution} mechanism for a named
+    # fact. This does not distinguish between adding a new fact and adding
+    # a new way to resolve a fact.
+    #
+    # @overload add(name, options = {}, { || ... })
+    # @param name [String] the fact name
+    # @param options [Hash] optional parameters for the fact - attributes
+    #   of {RFacter::Util::Fact} and {Facter::Util::Resolution} can be
+    #   supplied here
+    # @option options [Integer] :timeout set the
+    #   {RFacter::Util::Resolution#timeout timeout} for this resolution
+    # @param block [Proc] a block defining a fact resolution
+    #
+    # @return [RFacter::Util::Fact] the fact object, which includes any previously
+    #   defined resolutions
     def self.add(name, options = {}, &block)
       COLLECTION.value.add(name, options, &block)
     end
 
+    # Clears all cached values and removes all facts from memory.
+    #
+    # @return [void]
+    def self.clear
+      self.flush
+      self.reset
+    end
+
+    # Prints a debug message if debugging is turned on
+    #
+    # @param msg [String] the debug message
+    #
+    # @return [void]
     def self.debug(msg)
       ::RFacter::Config.config.logger.debug(msg)
     end
 
+    # Prints a debug message only once.
+    #
+    # @note Uniqueness is based on the string, not the specific location
+    #   of the method call.
+    #
+    # @param msg [String] the debug message
+    # @return [void]
     def self.debugonce(msg)
       ::RFacter::Config.config.logger.debugonce(msg)
+    end
+
+    # Define a new fact or extend an existing fact.
+    #
+    # @param name [Symbol] The name of the fact to define
+    # @param options [Hash] A hash of options to set on the fact
+    #
+    # @return [RFacter::Util::Fact] The fact that was defined
+    #
+    # @see {RFacter::Util::Collection#define_fact}
+    def self.define_fact(name, options = {}, &block)
+      COLLECTION.value.define_fact(name, options, &block)
+    end
+
+    # Iterates over fact names and values
+    #
+    # @yieldparam [String] name the fact name
+    # @yieldparam [String] value the current value of the fact
+    #
+    # @return [void]
+    def self.each
+      COLLECTION.value.each do |*args|
+        yield(*args)
+      end
     end
 
     # (see [])
@@ -76,8 +126,59 @@ EOS
       COLLECTION.value.fact(name)
     end
 
+    # Flushes cached values for all facts. This does not cause code to be
+    # reloaded; it only clears the cached results.
+    #
+    # @return [void]
+    def self.flush
+      COLLECTION.value.flush
+    end
+
+    # Lists all fact names
+    #
+    # @return [Array<String>] array of fact names
+    def self.list
+      COLLECTION.value.list
+    end
+
+    # Loads all facts.
+    #
+    # @return [void]
+    def self.loadfacts
+      COLLECTION.value.load_all
+    end
+
     def self.log_exception(exception, message = nil)
       ::RFacter::Config.config.logger.log_exception(exception, messge)
+    end
+
+    # Removes all facts from memory. Use this when the fact code has
+    # changed on disk and needs to be reloaded.
+    #
+    # @note This is currently a no-op for RFacter pending changes to how
+    #   collections are handled.
+    #
+    # @return [void]
+    def self.reset
+    end
+
+    # Register directories to be searched for facts. The registered directories
+    # must be absolute paths or they will be ignored.
+    #
+    # @param dirs [String] directories to search
+    #
+    # @note This is currently a no-op for RFacter pending changes to how
+    #   collections are handled.
+    #
+    # @return [void]
+    def self.search(*dirs)
+    end
+
+    # Returns the registered search directories.
+    #
+    # @return [Array<String>] An array of the directories searched
+    def self.search_path
+      COLLECTION.value.search_path
     end
 
     # Gets a hash mapping fact names to their values.
@@ -97,10 +198,34 @@ EOS
       COLLECTION.value.value(name, NODE.value)
     end
 
+    # Returns the current RFacter version
+    #
+    # @return [String]
+    def self.version
+      RFacter::VERSION
+    end
+
+    # Prints a warning message. The message is only printed if debugging
+    # is enabled.
+    #
+    # @param msg [String] the warning message to be printed
+    #
+    # @return [void]
     def self.warn(msg)
       ::RFacter::Config.config.logger.warn(msg)
     end
 
+
+    # Prints a warning message only once per process. Each unique string
+    # is printed once.
+    #
+    # @note Unlike {warn} the message will be printed even if debugging is
+    #   not turned on. This behavior is likely to change and should not be
+    #   relied on.
+    #
+    # @param msg [String] the warning message to be printed
+    #
+    # @return [void]
     def self.warnonce(msg)
       ::RFacter::Config.config.logger.warnonce(msg)
     end
@@ -108,6 +233,7 @@ EOS
     # Facter::Core DSL methods
     module Core
       require_relative 'core/aggregate'
+      # (see RFacter::Core::Aggregate)
       Aggregate = ::RFacter::Core::Aggregate
 
       # Shims for Facter::Core::Exection methods
@@ -178,9 +304,11 @@ EOS
     # Facter::Util DSL methods
     module Util
       require_relative 'util/fact'
+      # (see RFacter::Util::Fact)
       Fact = ::RFacter::Util::Fact
 
       require_relative 'util/resolution'
+      # (see RFacter::Util::Resolution)
       Resolution = ::RFacter::Util::Resolution
 
       # Methods for interacting with remote files.
