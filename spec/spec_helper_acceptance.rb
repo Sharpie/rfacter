8a79dc6f030b80a3c9af36c07790115e7c01aa07
Charlie Sharpsteen <source@sharpsteen.net>
2017-05-15 11:21:29 -0500

    Improve default nodeset for acceptance tests
    This patch switches the default nodeset from RedHat variants to a selection
    of commercially supported Linux variants:
    
      - centos:centos7
      - centos:centos6
      - ubuntu:16.04
      - opensuse:13.3 (Should be SLES, but this is what we got)

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,3 +1,5 @@
-ENV['BEAKER_set'] ||= 'docker/redhat'
+# See spec/acceptance/nodesets/docker for other options
+# that can be passed in via BEAKER_set.
+ENV['BEAKER_set'] ||= 'docker/default'
 
 require 'beaker-rspec'
