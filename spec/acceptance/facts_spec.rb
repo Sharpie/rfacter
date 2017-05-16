046dcfdc22e89b05169366158681bfb1ab12244b
Charlie Sharpsteen <source@sharpsteen.net>
2017-05-15 23:25:41 -0500

    Add acceptance tests for the os fact
    This patch adds acceptance tests covering resolutions of the os fact on various
    Linux platforms.

--- spec/acceptance/facts_spec.rb
+++ spec/acceptance/facts_spec.rb
@@ -1,7 +1,6 @@
 require 'spec_helper_acceptance'
 
 require 'cgi'
-require 'pp'
 
 require 'rfacter/node'
 require 'rfacter/util/collection'
@@ -28,11 +27,99 @@ describe RFacter do
     end
   end
 
-  it 'collects facts from remote nodes' do
-    hosts.each do |host|
-      expect(@facts).to include(host.hostname)
-    end
+  hosts.each do |host|
+    context "when collecting facts from #{host.hostname}" do
+      # The testcases below are assembled by matching the host platform against
+      # beaker platofrm specifications and the host hostname against
+      # beaker-hostgenerator platform strings.
+      context('resolving os.family') do
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'family' => 'RedHat')))
+        end if [/^el-/, /^centos-/, /^redhat-/, /^oracle-/, /^scientific-/, /^fedora-/].any? {|regex| regex.match(host.platform)}
 
-    puts PP.pp(@facts, '')
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'family' => 'Suse')))
+        end if [/^sles-/, /^opensuse/].any? {|regex| regex.match(host.platform)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'family' => 'Debian')))
+        end if [/^debian-/, /^ubuntu/].any? {|regex| regex.match(host.platform)}
+      end # os.family examples
+
+
+      describe('resolving os.name') do
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'Fedora')))
+        end if [/^fedora\d+/].any? {|regex| regex.match(host.hostname)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'RedHat')))
+        end  if [/^redhat\d+/].any? {|regex| regex.match(host.hostname)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'CentOS')))
+        end  if [/^centos\d+/].any? {|regex| regex.match(host.hostname)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'OracleLinux')))
+        end if [/^oracle\d+/].any? {|regex| regex.match(host.hostname)}
+
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'Debian')))
+        end if [/^debian\d+/].any? {|regex| regex.match(host.hostname)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'Ubuntu')))
+        end if [/^ubuntu\d+/].any? {|regex| regex.match(host.hostname)}
+
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'OpenSuSE')))
+        end if [/^opensuse\d+/].any? {|regex| regex.match(host.hostname)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+            'name' => 'SLES')))
+        end if [/^sles\d+/].any? {|regex| regex.match(host.hostname)}
+      end # os.name examples
+
+      describe('resolving os.architecture') do
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+              'architecture' => 'x86_64',
+              'hardware' => 'x86_64')))
+        end if [/-x86_64$/].any? {|regex| regex.match(host.platform)}
+
+        it do
+          expect(@facts[host.hostname]).to match(
+            a_hash_including('os' => a_hash_including(
+              'architecture' => 'amd64',
+              'hardware' => 'x86_64')))
+        end if [/-amd64$/].any? {|regex| regex.match(host.platform)}
+      end
+    end
   end
 end
